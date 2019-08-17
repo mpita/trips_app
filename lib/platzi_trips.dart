da17940cace303cfb0a03d04c5c6e15f6926bf78
Manuel Pita <mpita1984@gmail.com>
2019-08-17 02:56:47 +0200

    Generando Navegación en BottomNavigationBar

--- lib/platzi_trips.dart
+++ lib/platzi_trips.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home_trips.dart';
-
+import 'search_trips.dart';
+import 'profile_trips.dart';
 
 class PlatziTrips extends StatefulWidget {
   @override
@@ -11,35 +12,53 @@ class PlatziTrips extends StatefulWidget {
 
 }
 
-
 class _PlatziTrips extends State<PlatziTrips> {
+  int indexTap = 0;
+
+  final List<Widget>  widgetsChildren = [
+    HomeTrips(),
+    SearchTrips(),
+    ProfileTrips()
+  ];
+
+  void onTapTapped(int index){
+
+    setState(() {
+      indexTap = index;
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
+
+    return  Scaffold(
+      body: widgetsChildren[indexTap],
       bottomNavigationBar: Theme(
         data: Theme.of(context).copyWith(
-          canvasColor: Colors.white,
-          primaryColor: Colors.purple
+            canvasColor: Colors.white,
+            primaryColor: Colors.purple
         ),
         child: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text("")
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                title: Text("")
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Text("")
-            ),
-          ]
+            onTap: onTapTapped,
+            currentIndex: indexTap,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  title: Text("")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  title: Text("")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  title: Text("")
+              ),
+            ]
         ),
       ),
-      body: HomeTrips()
     );
   }
 
